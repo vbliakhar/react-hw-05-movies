860d7dc901879064d5cfd389c72e1bef92ec511b
vbliakhar <vovabliahar@gmail.com>
2021-11-24 13:37:42 +0100

    add modul react-slick

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -4,6 +4,10 @@ import * as movieShellAPI from "../../services/movieShelf-api";
 import ImageError from "../ImageError/ImageError";
 import PropTypes from "prop-types";
 import loader from "../../services/loader";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import "./MovieCast.scss";
 
 const MovieCast = ({ movie }) => {
   const { moviesId } = useParams();
@@ -24,29 +28,43 @@ const MovieCast = ({ movie }) => {
         setLoader((isLoading) => !isLoading);
       });
   }, [moviesId]);
-  return (
-    <>
-      {isLoader && loader()}
-      {actors &&
-        actors.map(({ profile_path, name, character, cast_id }) => {
-          return (
-            <li key={cast_id}>
-              {profile_path ? (
-                <img
-                  src={`https://image.tmdb.org/t/p/original/${profile_path}`}
-                  alt=""
-                  width="150px"
-                />
-              ) : (
-                <ImageError />
-              )}
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    arrows: true,
+  };
 
-              <p>{name}</p>
-              <p>Character: {character ? character : `not specified`}</p>
-            </li>
-          );
-        })}
-    </>
+  return (
+    <div className="position-cast">
+      {isLoader && loader()}
+      {actors && (
+        <ul className="list">
+          <Slider {...settings} width="500px">
+            {actors.map(({ profile_path, name, character, cast_id }) => {
+              return (
+                <li key={cast_id} className="li-block">
+                  {profile_path ? (
+                    <img
+                      src={`https://image.tmdb.org/t/p/original/${profile_path}`}
+                      alt=""
+                      width="150px"
+                    />
+                  ) : (
+                    <ImageError />
+                  )}
+
+                  <p>{name}</p>
+                  <p>Character: {character ? character : `not specified`}</p>
+                </li>
+              );
+            })}
+          </Slider>
+        </ul>
+      )}
+    </div>
   );
 };
 
